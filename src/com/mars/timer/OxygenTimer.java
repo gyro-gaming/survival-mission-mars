964355f1b35daf8ed7508410646b0b0614f42ad1
Shi-Tian Liu <liust92@gmail.com>
2022-05-12 08:35:38 -0700

    Add GameTimer, OxygenTimer and AbstractTimer AB#607 AB#616

--- src/com/mars/timer/OxygenTimer.java
+++ src/com/mars/timer/OxygenTimer.java
@@ -1,4 +1,21 @@
 package com.mars.timer;
 
-public class OxygenTimer {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class OxygenTimer extends AbstractTimer{
+    private long delay = 15 * 60000L;
+
+    public void pickUpOxygen() {
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+
+        try {
+            Date date = sdf.parse(stringFormEndTime);
+            Long newDate = date.getTime() + delay/1000;
+            stringFormEndTime = new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new java.util.Date (newDate*1000));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
 }
