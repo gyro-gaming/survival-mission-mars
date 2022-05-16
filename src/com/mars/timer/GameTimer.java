4609f5674f50420976bffa1d268c53870ee92b91
Shi-Tian Liu <liust92@gmail.com>
2022-05-16 13:21:53 -0700

    Fix a timer bug AB#617 AB#616

--- src/com/mars/timer/GameTimer.java
+++ src/com/mars/timer/GameTimer.java
@@ -1,12 +1,5 @@
 package com.mars.timer;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
-
 // time calculation method
 public class GameTimer extends AbstractTimer{
     private long delay = 60 * 60000L;
