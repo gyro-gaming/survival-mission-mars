2ee1561e1c3ea6bf40d8f81072ffcb250e35015c
Devin Braxton <dbraxton2621@gmail.com>
2022-05-16 11:29:19 -0500

    AB# 514 added the buttons back

--- src/com/mars/Main.java
+++ src/com/mars/Main.java
@@ -1,5 +1,6 @@
 package com.mars;
 
+import com.mars.client.Audio;
 import com.mars.client.CommandProcessor;
 import com.mars.client.Game;
 import com.mars.gui.SplashScreen;
@@ -7,14 +8,18 @@ import com.mars.timer.AbstractTimer;
 import com.mars.timer.GameTimer;
 import com.mars.timer.OxygenTimer;
 
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
 import java.util.Date;
 
 class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LineUnavailableException, UnsupportedAudioFileException, IOException {
         new SplashScreen();
-
-
-
+        Clip clip = AudioSystem.getClip();
+        Audio.playAudio(clip);
 
     }
 }
