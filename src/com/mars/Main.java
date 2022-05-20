df1e1ecef823633624bb744fe946ae10706a07d4
Jamal Hawkins <54287100+Jamal-prog@users.noreply.github.com>
2022-05-20 13:51:41 -0500

    AB#845 AB#846 created Winning and Losing Screens

--- src/com/mars/Main.java
+++ src/com/mars/Main.java
@@ -1,9 +1,13 @@
 package com.mars;
 
+import com.mars.gui.LosingScreen;
 import com.mars.gui.SplashScreen;
+import com.mars.gui.WinningScreen;
 
 class Main {
     public static void main(String[] args) {
         new SplashScreen();
+       //new WinningScreen();
+      // new LosingScreen();
     }
 }
