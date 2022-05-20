92a1867872d2c866ad91bc90978f5f16cf86392f
Shi-Tian Liu <liust92@gmail.com>
2022-05-20 15:25:44 -0700

    Test for Item Class AB#834 AB#831 AB#832

--- test/com/mars/items/FoodItemTest.java
+++ test/com/mars/items/FoodItemTest.java
@@ -1,17 +1,27 @@
 package com.mars.items;
 
+import com.mars.client.Game;
+import com.mars.locations.Room;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class FoodItemTest {
-
-    @Before
-    public void init() {
-    }
+    Item item = null;
+    Game game = Game.getInstance();
 
     @Test
     public void testEquals() {
+        for (Item i : Game.getItems()){
+            if (i.getName().equals("Bread")){
+                item = i;
+            }
+        }
+        for (Item i : Game.getItems()){
+            if (i.getName().equals("Bread")){
+                assertTrue(i.equals(item));
+            }
+        }
     }
 }
